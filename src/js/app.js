935c935846f15c9727734a0e9d1d47678aa20170
Abhishek.P <abhishek.p@talktalkplc.com>
2016-06-10 15:12:40 +0530

    added views

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,22 @@
 angular.module('todoApp',[
 	'ngRoute',
 	'todoApp.controllers'
-]);
+]).config(function($routeProvider) {
+	$routeProvider
+
+	.when('/', {
+		templateUrl: 'src/views/home.html' 
+	})
+
+	.when('/today', {
+		templateUrl: 'src/views/today.html'
+	})
+
+	.when('/all', {
+		templateUrl: 'src/views/all.html'
+	})
+
+	.when('/done', {
+		templateUrl: 'src/views/done.html'
+	});
+});
