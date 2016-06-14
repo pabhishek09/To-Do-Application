6f763ee2e68dd67d772d545a2f1311899128c372
Abhishek.P <abhishek.p@talktalkplc.com>
2016-06-14 15:52:18 +0530

    added condition for local storage

--- src/js/services.js
+++ src/js/services.js
@@ -1,9 +1,11 @@
 angular.module('todoApp.services', [
 ]).service('todoFactory',[function() {
 	var sessionTodoList = new Array();
-	var previousTodos = JSON.parse(localStorage['todoList']);
-	for (todo in previousTodos) {
-		sessionTodoList.push(previousTodos[todo]);
+		if (localStorage['todoList']) {
+			var previousTodos = JSON.parse(localStorage['todoList']);
+		for (todo in previousTodos) {
+			sessionTodoList.push(previousTodos[todo]);
+		}
 	}
 
 	this.addTodo = function(task) {
